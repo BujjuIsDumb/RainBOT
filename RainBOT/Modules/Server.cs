a7624fde3ff33dcc6b1b0aff671ab79774a37495
BujjuIsDumb <91763194+BujjuIsDumb@users.noreply.github.com>
2022-12-02 11:37:16 -0600

    Prepare main menu for new settings menus

--- RainBOT/Modules/Server.cs
+++ RainBOT/Modules/Server.cs
@@ -65,252 +65,25 @@ namespace RainBOT.Modules
         [SlashRequireGuildAccount]
         public async Task ServerSettingsAsync(InteractionContext ctx)
         {
-            // Format list settings.
-            var ventModerators = new StringBuilder();
-            ctx.Guild.GetGuildAccount(Data).VentModerators.ToList().ForEach(async x => ventModerators.AppendLine((await ctx.Client.GetUserAsync(x)).Mention));
-            if (string.IsNullOrEmpty(ventModerators.ToString())) ventModerators.Append("None");
-
-            var verificationForm = new StringBuilder();
-            ctx.Guild.GetGuildAccount(Data).VerificationFormQuestions.ToList().ForEach(x => verificationForm.AppendLine(x));
-            if (string.IsNullOrEmpty(verificationForm.ToString())) verificationForm.Append("None");
-
-            // Build main menu components.
+            // Build main menu.
             var mainEmbed = new DiscordEmbedBuilder()
                 .WithTitle("Server Settings")
                 .WithThumbnail(ctx.Guild.IconUrl)
                 .WithDescription("Manage the settings of your RainBOT server account. Please select a module to configure.")
-                .WithColor(new DiscordColor(3092790))
-                .AddField("Vent Moderators", ventModerators.ToString())
-                .AddField("Anonymous Venting", ctx.Guild.GetGuildAccount(Data).AnonymousVenting ? "Enabled" : "Disabled")
-                .AddField("Delete Verification Requests", ctx.Guild.GetGuildAccount(Data).DeleteVerificationRequests ? "Enabled" : "Disabled")
-                .AddField("Create Vetting Thread", ctx.Guild.GetGuildAccount(Data).CreateVettingThread ? "Enabled" : "Disabled")
-                .AddField("Verification Form", verificationForm.ToString());
+                .WithColor(new DiscordColor(3092790));
 
             var ventingButton = new DiscordButtonComponent(ButtonStyle.Secondary, Core.Utilities.CreateCustomId("ventingButton"), "Venting", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("👁‍🗨")));
-            var verificationButton = new DiscordButtonComponent(ButtonStyle.Secondary, Core.Utilities.CreateCustomId("verificationButton"), "Verification", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("✅")));
+            var bioButton = new DiscordButtonComponent(ButtonStyle.Secondary, Core.Utilities.CreateCustomId("bioButton"), "Bio", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("📜")));
 
-            var mainResponse = new DiscordInteractionResponseBuilder()
+            await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder()
                 .AddEmbed(mainEmbed)
-                .AddComponents(ventingButton, verificationButton)
-                .AsEphemeral();
-
-            // Build venting menu components.
-            var ventingEmbed = new DiscordEmbedBuilder()
-                .WithAuthor("Server Settings", null, ctx.Guild.IconUrl)
-                .WithTitle("Venting Settings")
-                .WithDescription("Configure the venting system.")
-                .WithColor(new DiscordColor(3092790))
-                .AddField("Vent Moderators", ventModerators.ToString())
-                .AddField("Anonymous Venting", ctx.Guild.GetGuildAccount(Data).AnonymousVenting ? "Enabled" : "Disabled");
-
-            var ventModeratorsButton = new DiscordButtonComponent(ButtonStyle.Secondary, Core.Utilities.CreateCustomId("ventModeratorsButton"), "Vent Moderators", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("🛡")));
-            var anonymousVentingButton = new DiscordButtonComponent(ButtonStyle.Secondary, Core.Utilities.CreateCustomId("anonymousVentingButton"), "Allow Anonymous Vents", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("😷")));
-
-            var ventingResponse = new DiscordInteractionResponseBuilder()
-                .AddEmbed(ventingEmbed)
-                .AddComponents(ventModeratorsButton, anonymousVentingButton)
-                .AsEphemeral();
-
-            // Build verification menu components.
-            var verificationEmbed = new DiscordEmbedBuilder()
-                .WithAuthor("Server Settings", null, ctx.Guild.IconUrl)
-                .WithTitle("Verification Settings")
-                .WithDescription("Configure the verification system.")
-                .WithColor(new DiscordColor(3092790))
-                .AddField("Delete Verification Requests", ctx.Guild.GetGuildAccount(Data).DeleteVerificationRequests ? "Enabled" : "Disabled")
-                .AddField("Create Vetting Thread", ctx.Guild.GetGuildAccount(Data).CreateVettingThread ? "Enabled" : "Disabled")
-                .AddField("Verification Form", verificationForm.ToString());
-
-            var deleteVerificationRequestsButton = new DiscordButtonComponent(ButtonStyle.Secondary, Core.Utilities.CreateCustomId("deleteVerificationRequestsButton"), "Delete Verification Requests", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("🗑")));
-            var createVettingThreadButton = new DiscordButtonComponent(ButtonStyle.Secondary, Core.Utilities.CreateCustomId("createVettingThreadButton"), "Create Vetting Thread", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("⚖️")));
-            var verificationFormButton = new DiscordButtonComponent(ButtonStyle.Secondary, Core.Utilities.CreateCustomId("verificationFormButton"), "Verification Form", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("❓")));
-
-            var verificationResponse = new DiscordInteractionResponseBuilder()
-                .AddEmbed(verificationEmbed)
-                .AddComponents(deleteVerificationRequestsButton, createVettingThreadButton, verificationFormButton)
-                .AsEphemeral();
-
-            // Send main menu message.
-            await ctx.CreateResponseAsync(mainResponse);
+                .AddComponents(ventingButton, bioButton)
+                .AsEphemeral());
 
             // Respond to button input.
             ctx.Client.ComponentInteractionCreated += async (sender, args) =>
             {
-                if (args.Id == ventingButton.CustomId)
-                {
-                    await ctx.DeleteResponseAsync();
-                    await args.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, ventingResponse);
-
-                    var ventingInteraction = args.Interaction;
-
-                    // Respond to button input.
-                    ctx.Client.ComponentInteractionCreated += async (sender, args) =>
-                    {
-                        if (args.Id == ventModeratorsButton.CustomId)
-                        {
-                            await ventingInteraction.DeleteOriginalResponseAsync();
-
-                            var ventModeratorsSelect = new DiscordUserSelectComponent(Core.Utilities.CreateCustomId("ventModeratorsSelect"), "Select users", false, 1, 25);
-
-                            await args.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                                .WithContent("Please select the people who you want to be able to moderate anonymous vents.")
-                                .AddComponents(ventModeratorsSelect)
-                                .AsEphemeral());
-
-                            // Respond to select menu input.
-                            ctx.Client.ComponentInteractionCreated += async (sender, args) =>
-                            {
-                                if (args.Id == ventModeratorsSelect.CustomId)
-                                {
-                                    var selectedUsers = new List<ulong>();
-                                    foreach (string userId in args.Values)
-                                    {
-                                        selectedUsers.Add(Convert.ToUInt64(userId));
-                                    }
-
-                                    ctx.Guild.GetGuildAccount(Data).VentModerators = selectedUsers.ToArray();
-                                    Data.Update();
-
-                                    await args.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                                        .WithContent("✅ The setting has been set.")
-                                        .AsEphemeral());
-                                }
-                            };
-                        }
-                        else if (args.Id == anonymousVentingButton.CustomId)
-                        {
-                            await ventingInteraction.DeleteOriginalResponseAsync();
-
-                            var anonymousVentingSelect = new DiscordSelectComponent(Core.Utilities.CreateCustomId("anonymousVentingSelect"), "Select an option", new List<DiscordSelectComponentOption>()
-                            {
-                                new DiscordSelectComponentOption("Yes", "yes", "Allow users to create anonymous vents.", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("✅"))),
-                                new DiscordSelectComponentOption("No", "no", "Do not allow users to create anonymous vents.", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("🚫")))
-                            });
-
-                            await args.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                                .WithContent("Please select whether or not you want people to be able to create anonymous vents.")
-                                .AddComponents(anonymousVentingSelect)
-                                .AsEphemeral());
-
-                            // Respond to select menu input.
-                            ctx.Client.ComponentInteractionCreated += async (sender, args) =>
-                            {
-                                if (args.Id == anonymousVentingSelect.CustomId)
-                                {
-                                    ctx.Guild.GetGuildAccount(Data).AnonymousVenting = args.Values.First() == "yes";
-                                    Data.Update();
-
-                                    await args.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                                        .WithContent("✅ The setting has been set.")
-                                        .AsEphemeral());
-                                }
-                            };
-                        }
-                    };
-                }
-                else if (args.Id == verificationButton.CustomId)
-                {
-                    await ctx.DeleteResponseAsync();
-                    await args.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, verificationResponse);
-
-                    var verificationInteraction = args.Interaction;
-
-                    // Respond to button input.
-                    ctx.Client.ComponentInteractionCreated += async (sender, args) =>
-                    {
-                        if (args.Id == deleteVerificationRequestsButton.CustomId)
-                        {
-                            await verificationInteraction.DeleteOriginalResponseAsync();
-
-                            var deleteVerificationRequestsSelect = new DiscordSelectComponent(Core.Utilities.CreateCustomId("deleteVerificationRequestsSelect"), "Select an option", new List<DiscordSelectComponentOption>()
-                            {
-                                new DiscordSelectComponentOption("Yes", "yes", "Delete verification requests after they're accepted/denied.", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("✅"))),
-                                new DiscordSelectComponentOption("No", "no", "Do not delete verification requests after they're accepted/denied.", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("🚫")))
-                            });
-
-                            await args.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                                .WithContent("Please select whether or not to delete verification requests after they're accepted/denied.")
-                                .AddComponents(deleteVerificationRequestsSelect)
-                                .AsEphemeral());
-
-                            // Respond to select menu input.
-                            ctx.Client.ComponentInteractionCreated += async (sender, args) =>
-                            {
-                                if (args.Id == deleteVerificationRequestsSelect.CustomId)
-                                {
-                                    ctx.Guild.GetGuildAccount(Data).DeleteVerificationRequests = args.Values.First() == "yes";
-                                    Data.Update();
-
-                                    await args.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                                        .WithContent("✅ The setting has been set.")
-                                        .AsEphemeral());
-                                }
-                            };
-                        }
-                        else if (args.Id == createVettingThreadButton.CustomId)
-                        {
-                            await verificationInteraction.DeleteOriginalResponseAsync();
-
-                            var createVettingThreadSelect = new DiscordSelectComponent(Core.Utilities.CreateCustomId("createVettingThreadSelect"), "Select an option", new List<DiscordSelectComponentOption>()
-                            {
-                                new DiscordSelectComponentOption("Yes", "yes", "Create vetting threads for verification requests.", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("✅"))),
-                                new DiscordSelectComponentOption("No", "no", "Do not create vetting threads for verification requests.", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("🚫")))
-                            });
-
-                            await args.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                                .WithContent("Please select whether or not to create vetting threads for verification requests.")
-                                .AddComponents(createVettingThreadSelect)
-                                .AsEphemeral());
-
-                            // Respond to select menu input.
-                            ctx.Client.ComponentInteractionCreated += async (sender, args) =>
-                            {
-                                if (args.Id == createVettingThreadSelect.CustomId)
-                                {
-                                    ctx.Guild.GetGuildAccount(Data).CreateVettingThread = args.Values.First() == "yes";
-                                    Data.Update();
-
-                                    await args.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                                        .WithContent("✅ The setting has been set.")
-                                        .AsEphemeral());
-                                }
-                            };
-                        }
-                        else if (args.Id == verificationFormButton.CustomId)
-                        {
-                            await verificationInteraction.DeleteOriginalResponseAsync();
-
-                            // Build modal.
-                            var verificationFormModal = new DiscordInteractionResponseBuilder()
-                                .WithTitle("Create the verification form")
-                                .WithCustomId(Core.Utilities.CreateCustomId("verificationFormModal"))
-                                .AddComponents(new TextInputComponent(label: "Question 1", customId: "question1", required: false, style: TextInputStyle.Short, min_length: 5, max_length: 30))
-                                .AddComponents(new TextInputComponent(label: "Question 2", customId: "question2", required: false, style: TextInputStyle.Short, min_length: 5, max_length: 30))
-                                .AddComponents(new TextInputComponent(label: "Question 3", customId: "question3", required: false, style: TextInputStyle.Short, min_length: 5, max_length: 30))
-                                .AddComponents(new TextInputComponent(label: "Question 4", customId: "question4", required: false, style: TextInputStyle.Short, min_length: 5, max_length: 30));
-
-                            await args.Interaction.CreateResponseAsync(InteractionResponseType.Modal, verificationFormModal);
-
-                            // Respond to modal submission.
-                            ctx.Client.ModalSubmitted += async (sender, args) =>
-                            {
-                                if (args.Interaction.Data.CustomId == verificationFormModal.CustomId)
-                                {
-                                    var formQuestions = new List<string>();
-                                    foreach (string question in args.Values.Values)
-                                        if (!string.IsNullOrEmpty(question)) formQuestions.Add(question);
-
-                                    ctx.Guild.GetGuildAccount(Data).VerificationFormQuestions = formQuestions.ToArray();
-                                    Data.Update();
-
-                                    await args.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                                        .WithContent("✅ The setting has been set.")
-                                        .AsEphemeral());
-                                }
-                            };
-                        }
-                    };
-                }
+                // Unfinished
             };
         }
 
